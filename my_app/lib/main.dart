ba296d90ad40698dc6ab54a75b715311fc4981cd
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-02-27 11:48:08 +0900

    [update]#3 画面の更新(statefulWidget/setState)
    [タスク]初級編2:画面の更新(statefulWidget/setState)

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,33 +22,24 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _type = '偶数';
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      if (_counter % 2 == 0) {
+        _type = '偶数';
+      } else {
+        _type = '奇数';
+      }
     });
   }
 
@@ -71,34 +53,27 @@ class _MyHomePageState extends State<MyHomePage> {
         ],)
       ),
       drawer: const Drawer(child: Center(child: Text("Drawer"))),
-      body: Column(children: [
-        const Text('初めてのテキスト'),
-        const Text('初めてのテキスト'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const[
-              Icon(
-                Icons.favorite,
-                color: Colors.pink,
-                size: 24.0,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+              Text(
+                '$_counter', 
+                style: Theme.of(context).textTheme.headline4,
               ),
-              Icon(
-                Icons.audiotrack,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              Icon(
-                Icons.beach_access,
-                color: Colors.blue,
-                size: 36.0,
-              )
-            ],
-          ),
-        ]
-      ),
+              if (_counter % 2 == 0) const Text('偶数です', style: TextStyle(fontSize: 20, color: Colors.red)),
+              if(_counter % 2 == 1) const Text('奇数です', style: const TextStyle(fontSize: 20, color: Colors.red)),
+          ],
+        ),
+      ),  
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {print('押したね？')},
-        child: const Icon(Icons.timer),
+        onPressed: () => {
+          _incrementCounter(),
+          print('押したね？')
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
