8d741571dcb859b875d45a5cdea20b146c4b0efb
yuta.nishimura <yuta.nishimura@optim.co.jp>
2022-03-05 10:18:20 +0900

    [update]#4 AnimatedOpacity,AnimatedSize,AnimatedAlignを追加
    [タスク]初級編をクリアする

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -37,15 +37,15 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _type = '偶数';
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+  bool flag = false;
 
-  void _incrementCounter() {
+  _click() async {
     setState(() {
-      _counter++;
+      flag = !flag;
     });
   }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +58,33 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       drawer: const Drawer(child: Center(child: Text("Drawer"))),
       body: Center(
-        child: TestPage1(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedOpacity(
+                opacity: flag ? 0.1 : 1.0,
+                duration: const Duration(seconds: 3),
+                child: Text(
+                  "消える文字",
+                  style: Theme.of(context).textTheme.headline4,
+                )),
+            AnimatedSize(
+                vsync: this,
+                duration: const Duration(seconds: 3),
+                child: SizedBox(
+                    width: flag ? 50 : 200,
+                    height: flag ? 50 : 200,
+                    child: Container(color: Colors.purple))),
+            AnimatedAlign(
+                duration: const Duration(seconds: 3),
+                alignment: flag ? Alignment.topLeft : Alignment.bottomRight,
+                child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Container(color: Colors.green)))
+          ],
+        ),
+        // child: TestPage1(),
         // child: IconButton(
         //   icon: const Icon(Icons.open_in_browser),
         //   onPressed: () async{
@@ -72,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         key: const Key('increment'),
         onPressed: () => {
-          _incrementCounter(),
+          _click,
           print('押した')
         },
         tooltip: 'Increment',
