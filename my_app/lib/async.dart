938239d7492681365a48ed4a9a2e2b547956d811
p238049y <p238049y@gmail.com>
2022-03-07 22:41:25 +0900

    [add]#5 asyncFuncという非同期メソッドを実装してみる
    [タスク] 中級編1:非同期処理

---
+++ my_app/lib/async.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+class Async {
+  void asynctest2() {
+    print("method begin");
+    print(DateTime.now().toString());
+    print("data1 start");
+    print(asyncFunc("data1", 3));
+    print("data2 start");
+    print(asyncFunc("data2", 2));
+    print("data3 start");
+    print(asyncFunc("data3", 1));
+  }
+  // timeの時間分スリープし、その後現在時間を返す関数
+  Future<String> asyncFunc(String name, int time) async {
+    return Future.delayed(Duration(seconds: time), () {
+      return name + ":" + DateTime.now().toString();
+    });
+  }
+}
