a608616c92f3d3d4fa4c482c8d560922ce314b9d
p238049y <p238049y@gmail.com>
2022-06-17 12:41:52 +0900

    [add]#11 設定画面に入力フォームを追加

--- my_app/lib/Setting/SettingPage.dart
+++ my_app/lib/Setting/SettingPage.dart
@@ -8,11 +8,29 @@ class SettingPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('設定画面'),
       ),
-      body: Center(child: TextButton(child: const Text('ホームに戻る'),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: const [
+            TextField(
+              decoration: InputDecoration(
+                hintText: '名前',
+              ),
+              autofocus: true,
+            ), 
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'ふりがな',
+              ),
+            ),
+          ],
+        ),
+      )
+      //   Center(child: TextButton(child: const Text('ホームに戻る'),
+      //   onPressed: () {
+      //     Navigator.pop(context);
+      //   },
+      // )),
     );
   }
 
